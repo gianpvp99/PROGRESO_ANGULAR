3e3504c1d8ef1ae316a78e71a8f041af1fb61633
gianpvp99 <gianpvp99@gmail.com>
2023-10-14 10:27:46 -0500

    progresooo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import '@angular/localize/init';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ejemplo_version_ultima';
+  public progressbarHeight = '.857rem';
 }
